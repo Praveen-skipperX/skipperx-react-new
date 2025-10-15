bb1d580a7c6f1cccb8650ff366570283614c2323
Abbas Haider <developers@techglobiz.com>
2025-10-15 11:59:19 +0530

    New pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 
+import About from './pages/About';
+import ArVr from './pages/Ar-Vr';
+import Blog from './pages/blog';
+import Contact from './pages/contact';
+import Cooking from './pages/cooking';
+import CreatorPreneur from './pages/CreatorPreneur';
+import CreatorsHub from './pages/CreatorsHub';
+import DroneEngineering from './pages/DroneEngineering';
+import Email from './pages/email';
 import Home from './pages/Home';
-import TermsAndConditions from './pages/t & c'
+import Login from './pages/login';
+import Otp from './pages/otp';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import RefundPolicy from './pages/RefundPolicy';
-import About from './pages/About';
-import Blog from './pages/blog';
-import DroneEngineering from './pages/DroneEngineering';
 import RobotEngineering from './pages/RobotEngineering';
-import ArVr from './pages/Ar-Vr';
 import StartupStack from './pages/StartupStack';
+import TermsAndConditions from './pages/t & c';
 import TechCore from './pages/TechCore';
-import CreatorsHub from './pages/CreatorsHub';
-import Login from './pages/login';
-import Email from './pages/email';
-import Contact from './pages/contact';
-import Otp from './pages/otp';
-import Cooking from './pages/cooking';
 //import Forget from './pages/forget';
 
 import Dashboard from './components/dashboard/Dashboard';
@@ -45,6 +45,7 @@ function App() {
         <Route path="/Startup-Stack" element={<StartupStack />} />
         <Route path="/Core-Tech" element={<TechCore />} />
         <Route path="/creators-hub" element={<CreatorsHub />} />
+        <Route path="/creatorpreneur" element={<CreatorPreneur />} />
         <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/email" element={<Email />} />
